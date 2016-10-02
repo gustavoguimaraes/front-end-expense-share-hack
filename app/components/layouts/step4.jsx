99a80777a3758b7d32d1281ba96bdc76eac14994
Johannes <johannes.hildingsson@klickrent.com>
2016-10-02 14:41:40 +0200

    step 4 fix

--- app/components/layouts/step4.jsx
+++ app/components/layouts/step4.jsx
@@ -17,6 +17,8 @@ const Step4 = () => {
       <div className="alert alert-success">
         <h3>Your debts are now settled and automatically transfered to your accounts!</h3>
         <h3>Happy traveling in the future </h3>
+        <br />
+        <h4><span className="glyphicon glyphicon-envelope"></span>Check your email for a status report of this xPense trip report</h4>
       </div>
     </div>
     </div>
