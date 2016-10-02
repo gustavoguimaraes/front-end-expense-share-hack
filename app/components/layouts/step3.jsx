bea12dba69ebc955a822c7f827eaa5713ee21548
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2016-10-02 14:19:19 +0200

    Step 3

--- app/components/layouts/step3.jsx
+++ app/components/layouts/step3.jsx
@@ -12,36 +12,33 @@ class Step3 extends React.Component {
         <div>
           <Navbar />
         </div>
-        <div className="row">
-         <div className="col-md-2">
-         </div>
-         <div className="col-md-8">
-         <div className="infoBoxes">
-         </div>
-      <div className='step-3'>
-        <div className='settlement'>
-          <h3>You have traveled to: <b className='travelInfoLink'>Berlin</b></h3>
-          <div>
-            <h2>Select amount</h2>
-            <h4>Total budget 150 € :</h4>
-            <h4>Total budget 130 € was spent</h4>
+        <div className='row'>
+          <div className='col-md-2'>
+          </div>
+          <div className='col-md-8'>
+            <div className='infoBoxes'>
+            </div>
+            <div className='step-3'>
+              <div className='settlement'>
+                <h3>You have traveled to: <b className='travelInfoLink'>Berlin</b></h3>
+                <div>
+                  <h2>Select amount</h2>
+                  <h4>Total budget 150 € :</h4>
+                  <h4>Total budget 130 € was spent</h4>
 
-            <h4>Settle date</h4>
-          <h5>  2016-10-13 </h5>
-            <div className="col-md-8">
-
-            Results:
-            <ListFriends name='Evelyn' /> 10+
-            <ListFriends name='T-rex' /> 10+
-            <br />
-            <button className='btn btn-success btn-lg'>Review and accept</button>
+                  <h4>Settle date</h4>
+                  <h5>2016-10-13</h5>
+                  <div className='col-md-8'>Results:
+                    <ListFriends name='Evelyn' /> 10+
+                    <ListFriends name='T-rex' /> 10+
+                    <br />
+                    <Link to={ '/step-4' }> <div className='btn btn-success btn-lg'>Review and accept</div></Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      </div>
-      </div>
       </div>
     );
   }
